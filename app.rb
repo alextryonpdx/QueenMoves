26f85da45e0e78f5c9e38e1371f3701484e8f696
alextryonpdx <alextryonpdx@gmail.com>
2017-03-22 12:46:38 -0700

    first commit

---
+++ app.rb
@@ -0,0 +1,22 @@
+require('sinatra')
+require('sinatra/reloader')
+require('pry')
+also_reload('lib/**/*.rb')
+require('./lib/queen_moves')
+
+
+get('/form') do
+  erb(:form)
+end
+
+
+get('/result') do
+  @coords = [[params.fetch('start-x-coord').to_i,
+			   params.fetch('start-y-coord').to_i],
+			  [params.fetch('end-x-coord').to_i,
+			   params.fetch('end-y-coord').to_i]
+			   ]
+# binding.pry
+  @validity = @coords.queen_move?
+  erb(:result)
+end
